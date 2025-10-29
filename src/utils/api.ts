d7dff3aa8cbe803461d78fdff488409f17fe9a9f
irawanrafidzufar <irawanrafidzufar@gmail.com>
2025-10-29 14:19:46 +0700

    api configuration

---
+++ src/utils/api.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import {cookies} from "next/headers";
+
+const BASE_URL = 'https://kb8334ks-3000.asse.devtunnels.ms/'
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+api.interceptors.request.use(
+    (config) => {
+        const cookieStore = cookies()
+
+        // @ts-ignore
+        const token = cookieStore.get('auth-token')?.value
+
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default api
