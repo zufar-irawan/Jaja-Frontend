16304a4017105b8c975d832dc1bde32b10d4deca
irawanrafidzufar <irawanrafidzufar@gmail.com>
2025-11-07 10:45:55 +0700

    Image profile Modal

--- src/app/clientArea/profile/page.tsx
+++ src/app/clientArea/profile/page.tsx
@@ -2,9 +2,11 @@
 
 import { useState } from "react";
 import ProfileEditModal from "./profileEditModal";
+import ProfileImageModal from "./profileImageModal";
 
 export default function ProfilePage() {
     const [modalEditProfile, setModalEditProfile] = useState(false);
+    const [modalEditImage, setModalEditImage] = useState(false);
 
     return (
         <div className="flex flex-col w-full gap-6">
@@ -99,7 +101,9 @@ export default function ProfilePage() {
                                     Edit Profil
                                 </button>
 
-                                <button className="w-full bg-white hover:bg-gray-50 text-gray-700 font-medium py-3 px-6 rounded-lg transition-all duration-200 border-2 border-gray-300 hover:border-blue-400 flex items-center justify-center gap-2">
+                                <button
+                                    className="w-full bg-white hover:bg-gray-50 text-gray-700 font-medium py-3 px-6 rounded-lg transition-all duration-200 border-2 border-gray-300 hover:border-blue-400 flex items-center justify-center gap-2"
+                                    onClick={() => setModalEditImage(true)}>
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                     </svg>
@@ -113,6 +117,10 @@ export default function ProfilePage() {
 
             {modalEditProfile && (
                 <ProfileEditModal onClose={setModalEditProfile} />
+            )}
+
+            {modalEditImage && (
+                <ProfileImageModal onClose={setModalEditImage} />
             )}
         </div>
     )
